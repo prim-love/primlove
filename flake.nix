244fca6bb0aa79c85e80b6c15f438f4ad37c9b97
indy <indynet00@gmail.com>
2026-03-19 14:30:55 -0500

    mk:initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    ox-blog.url     = "github:indynet/ox-blog";
+  };
+
+  outputs = { self, flake-utils, ox-blog, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = ox-blog.export.${system} {
+        name = "primlove";
+        src  = self;
+      };
+    });
+}
